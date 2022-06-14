b936800d1161e873d9d29eab3e79b93a3420f8b7
Juca P <88djony@gmail.com>
2022-06-13 19:04:09 -0700

    added some components and started on index.js and app.js

--- frd_app/src/App.js
+++ frd_app/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import Login from './';
+import About from './';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div>
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Login</Link>
+              </li>
+              <li>
+                <Link to="/">About</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path='/login' component= {Login}/>
+            <Route path='/about' component={About}/>
+          </Switch>
+        </div>
+      </Router>
     </div>
-  );
+  )
 }
 
 export default App;
